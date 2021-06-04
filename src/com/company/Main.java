2184b9fa551870ba715b7254331f30692b435f5f
SALBAROV <tynchtyk.salbarov@bv.ru>
2021-06-04 18:08:39 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int number = 2;
+        switch (number) {
+            case 1:
+                System.out.println("Число ровно одному");
+                break;
+            case 2:
+                System.out.println("Число ровно двум");
+                break;
+            case 3:
+                System.out.println("Число ровно трем");
+        }
+    }
+}
+
